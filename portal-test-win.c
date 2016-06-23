737f9c18c9d1a75b631b5a8e4fa15dcff1ada90a
Matthias Clasen <mclasen@redhat.com>
2016-06-22 23:38:49 -0400

    Add network monitor information

--- portal-test-win.c
+++ portal-test-win.c
@@ -7,6 +7,10 @@
 struct _PortalTestWin
 {
   GtkApplicationWindow parent;
+  GtkWidget *network_status;
+  GtkWidget *monitor_name;
+
+  GNetworkMonitor *monitor;
 };
 
 struct _PortalTestWinClass
@@ -16,11 +20,43 @@ struct _PortalTestWinClass
 
 G_DEFINE_TYPE (PortalTestWin, portal_test_win, GTK_TYPE_APPLICATION_WINDOW);
 
+static void
+update_network_status (PortalTestWin *win)
+{
+  g_autoptr(GString) s = g_string_new ("");
+  GEnumClass *class;
+  GEnumValue *value;
+
+  if (g_network_monitor_get_network_available (win->monitor))
+    g_string_append (s, "available");
+  if (g_network_monitor_get_network_metered (win->monitor))
+    {
+      if (s->len > 0)
+        g_string_append (s, ", ");
+      g_string_append (s, "metered");
+    }
+  class = g_type_class_ref (G_TYPE_NETWORK_CONNECTIVITY);
+  value = g_enum_get_value (class, g_network_monitor_get_connectivity (win->monitor));
+  if (s->len > 0)
+    g_string_append (s, ", ");
+  g_string_append_printf (s, "connectivity=%s", value->value_nick);
+  g_type_class_unref (class);
+
+  gtk_label_set_label (GTK_LABEL (win->network_status), s->str);
+}
 
 static void
 portal_test_win_init (PortalTestWin *win)
 {
+  g_autofree char *name = NULL;
+
   gtk_widget_init_template (GTK_WIDGET (win));
+
+  win->monitor = g_network_monitor_get_default ();
+  name = g_strdup_printf ("(%s)", G_OBJECT_TYPE_NAME (win->monitor));
+  gtk_label_set_label (GTK_LABEL (win->monitor_name), name);
+  g_signal_connect_swapped (win->monitor, "notify", G_CALLBACK (update_network_status), win);
+  update_network_status (win);
 }
 
 static gboolean
@@ -43,6 +79,8 @@ portal_test_win_class_init (PortalTestWinClass *class)
   gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (class),
                                                "/org/gtk/portal-test/portal-test-win.ui");
   gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (class), activate_link);
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), PortalTestWin, network_status);
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), PortalTestWin, monitor_name);
 }
 
 GtkApplicationWindow *
