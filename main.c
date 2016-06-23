a75429c9f376ed43d254f8062ea5af9fa898131f
Matthias Clasen <mclasen@redhat.com>
2016-06-22 22:39:02 -0400

    Add some diagnostics

--- main.c
+++ main.c
@@ -5,5 +5,7 @@
 int
 main (int argc, char *argv[])
 {
+  if (g_file_test ("/run/user/1000/flatpak-info", G_FILE_TEST_EXISTS))
+    g_print ("in a sandbox\n");
   return g_application_run (portal_test_app_new (), argc, argv);
 }
