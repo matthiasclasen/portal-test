07b7f7ceb2c4df32bc28999c649df9e889bb245f
Matthias Clasen <mclasen@redhat.com>
2018-11-26 17:03:20 -0500

    Flatpak build updates
    Bundle glib master for now, since we are using new API.
    And always install the built flatpak, since some of the
    functionality is only available from a sandbox.

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-flatpak-builder --force-clean --ccache --require-changes --repo=repo app org.gnome.PortalTest.json
+flatpak-builder --force-clean --ccache --install --repo=repo app org.gnome.PortalTest.json
